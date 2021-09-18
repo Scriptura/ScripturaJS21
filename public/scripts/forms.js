fd70bd9223ca8b6d8a79ae774bfc8b4b59b408fd
Olivier Chavarin <memoire1@gmail.com>
2021-09-18 21:42:50 +0200

    Regex revised

--- public/scripts/forms.js
+++ public/scripts/forms.js
@@ -10,46 +10,50 @@ document.querySelectorAll('[class*=regex]').forEach(input => {
 })
 
 function regexInit(input) {
-  let el = input.parentNode.querySelector('.error')
-  if (el) el.remove()
-  input.classList.add('invalid')
-  el = document.createElement('p')
-  el.classList.add('error')
-  el.classList.add('alert-warning')
-  el.style.display = 'none'
-  input.parentElement.appendChild(el)
+  let el = input.parentNode.querySelector('.alert-warning')
   if (input.classList.contains('regex-0')) regex0(input, el)
   if (input.classList.contains('regex-1')) regex1(input, el)
   if (input.classList.contains('regex-2')) regex2(input, el)
 }
 
+function createMessageError(input, el, text) {
+  el = input.parentNode.querySelector('.alert-warning') // Variable à réafecter.
+  input.classList.add('invalid')
+  if (el) el.remove()
+  el = document.createElement('p')
+  el.classList.add('alert-warning')
+  input.parentElement.appendChild(el)
+  el.textContent = text
+}
+
+function removeMessageError(input, el) {
+  input.classList.remove('invalid')
+  if (el) el.remove()
+}
+
 const regex0 = ((input, el) => {
-  if (input.value.length > 40) { // En principe 30 est suffisant...
-    el.textContent = "Champ invalide\u00a0: chaîne de caractères trop longue."
-    el.style.display = 'block'
+  if (input.value.length > 40) { // @todo En principe 30 est suffisant...
+    let text = "Champ invalide\u00a0: chaîne de caractères trop longue."
+    createMessageError(input, el, text)
+  } else {
+    removeMessageError(input, el)
   }
 })
 
 const regex1 = ((input, el) => {
   if (input.value.match('\\d')) {
-    el.textContent = "Champ invalide\u00a0: présence de caractères numériques."
-    el.style.display = 'block'
+    let text = "Champ invalide\u00a0: présence de caractères numériques."
+    createMessageError(input, el, text)
+  } else {
+    removeMessageError(input, el)
   }
 })
 
 const regex2 = ((input, el) => {
-  if (input.value.match('[\\\\/\\[\\]|%&!?\+÷×=_{}()<>;:,$€£¥¢*§@~`•√π¶∆^°²\#\"]')) { // Le point l'espace et les guillemets simples sont exclus du test.
-    el.textContent = "Champ invalide\u00a0: présence de caractères spéciaux non autorisés."
-    el.style.display = 'block'
+  if (input.value.match('[\\\\/\\[\\]|%&!?\+÷×=±_{}()<>;:,$€£¥¢*§@~`•√π¶∆^°²\#\"]')) { // Le point l'espace et les guillemets simples sont exclus du test.
+    let text = "Champ invalide\u00a0: présence de caractères spéciaux non autorisés."
+    createMessageError(input, el, text)
+  } else {
+    removeMessageError(input, el)
   }
 })
-
-/*
-const regexM = ((input, el) => {
-  const regex = '^[^A-ZÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð]'
-  if (input.value.match(regex)) {
-    el.textContent = "Champ invalide :une majuscule est requise."
-    el.style.display = 'block'
-  }
-})
-*/
