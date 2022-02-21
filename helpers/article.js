7f5fb12527f9371e317507b6557a4adf7affdcf5
Olivier Chavarin <memoire1@gmail.com>
2022-02-21 06:58:57 +0100

    Data author value add

--- helpers/article.js
+++ helpers/article.js
@@ -5,6 +5,9 @@ const vv = require('../settings/variables'),
 
 const articleFormat = (data) => { // Travaille sur l'initialisateur d'objet envoyé par la db et retourne des résultats formatés pour les besoins de la page.
   data._title = data._name.concat(' | ', vv.siteName)
+  if (data._given_name || data._family_name) {
+    data._author = [data._given_name, data._family_name].join(' ')
+  }
   if (!data._description) {
     data._description = data._name
     if (data._author) data._description = [data._description, data._author].join(', ')
