e77a8720e614479843ce3a5010afbc0b3bc28e04
Olivier Chavarin <memoire1@gmail.com>
2022-03-10 23:55:37 +0100

    Leaflet updated

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -21,17 +21,29 @@ const mapsIdAdd = (() => { // @note Affecter ou réafecter une id pour chaque ca
   })
 })()
 
+// @note Permet l'animation unique des marqueurs au lancement de la page.
+const startPage = (() => {
+  const html = document.documentElement,
+        c = 'start-map'
+  html.classList.add(c)
+  window.addEventListener('load', function() {
+    setTimeout(() => {
+      html.classList.remove(c)
+    }, 1500)
+  })
+})()
+
 const maps = (() => {
   document.querySelectorAll('.map').forEach(function(item) {
-    const id = item.id
     const map = () => {
-      const el = document.getElementById(id),
+      const el = document.getElementById(item.id),
             coords = JSON.parse(el.dataset.coords),
-            map = L.map(id).setView(coords, el.dataset.zoom)
+            map = L.map(item.id).setView(coords, el.dataset.zoom)
       L.tileLayer(
         el.dataset.tileserver,
         {attribution: el.dataset.attribution}
       ).addTo(map)
+      if (el.dataset.name) {
         let icon = L.icon({
           iconUrl: "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='40' height='40' viewBox='0 0 512 512'%3E%3Cpath fill='%23E74C3C' d='M256 14C146 14 57 102 57 211c0 172 199 295 199 295s199-120 199-295c0-109-89-197-199-197zm0 281a94 94 0 110-187 94 94 0 010 187z'/%3E%3Cpath fill='%23C0392B' d='M256 14v94a94 94 0 010 187v211s199-120 199-295c0-109-89-197-199-197z'/%3E%3C/svg%3E", // @see https://codepen.io/tigt/post/optimizing-svgs-in-data-uris @see https://codepen.io/jakob-e/pen/doMoML
           iconSize: [40, 40],
@@ -42,9 +54,10 @@ const maps = (() => {
           popupAnchor: [0, -60]
         })
         const marker = L.marker(coords, {icon: icon})
-        if (el.dataset.name) marker.bindPopup(el.dataset.name)
+        marker.bindPopup(el.dataset.name)
         marker.openPopup()
         marker.addTo(map)
+      }
       }
     window.addEventListener('load', function() {
       map()
