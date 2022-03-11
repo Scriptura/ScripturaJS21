db3d1c56dfcd286c9ef519bf2f7e1ce45d6cfa10
Olivier Chavarin <memoire1@gmail.com>
2022-03-11 11:03:48 +0100

    Leaflet updated

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -44,16 +44,14 @@ const maps = (() => {
         {attribution: el.dataset.attribution}
       ).addTo(map)
       if (el.dataset.name) {
-        let icon = L.icon({
-          iconUrl: "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='40' height='40' viewBox='0 0 512 512'%3E%3Cpath fill='%23E74C3C' d='M256 14C146 14 57 102 57 211c0 172 199 295 199 295s199-120 199-295c0-109-89-197-199-197zm0 281a94 94 0 110-187 94 94 0 010 187z'/%3E%3Cpath fill='%23C0392B' d='M256 14v94a94 94 0 010 187v211s199-120 199-295c0-109-89-197-199-197z'/%3E%3C/svg%3E", // @see https://codepen.io/tigt/post/optimizing-svgs-in-data-uris @see https://codepen.io/jakob-e/pen/doMoML
-          iconSize: [40, 40],
-          iconAnchor: [20, 40],
-          shadowUrl: '/libraries/leaflet/images/marker-shadow.png',
-          shadowSize: [50, 70],
-          shadowAnchor: [15, 70],
-          popupAnchor: [0, -60]
+        const divIcon = L.divIcon({
+          className: "leaflet-data-marker",
+          html: '<svg class="map-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#E74C3C" d="M256 14C146 14 57 102 57 211c0 172 199 295 199 295s199-120 199-295c0-109-89-197-199-197zm0 281a94 94 0 1 1 0-187 94 94 0 0 1 0 187z"/><path fill="#C0392B" d="M256 14v94a94 94 0 0 1 0 187v211s199-120 199-295c0-109-89-197-199-197z"/></svg>',
+          iconAnchor  : [20, 40],
+          iconSize    : [40, 40],
+          popupAnchor : [0, -60]
         })
-        const marker = L.marker(coords, {icon: icon})
+        const marker = L.marker(coords, {icon: divIcon})
         marker.bindPopup(el.dataset.name)
         marker.openPopup()
         marker.addTo(map)
