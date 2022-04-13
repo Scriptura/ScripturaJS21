65de3a0284f999e4a31d723ce7250f0303b86a55
Olivier Chavarin <memoire1@gmail.com>
2022-04-14 00:38:44 +0200

    Tabs revised

--- public/scripts/tab.js
+++ public/scripts/tab.js
@@ -11,89 +11,91 @@ const tabs = (() => {
   const addTablist = (() => {
     const tabss = document.querySelectorAll('.tabs')
     for (const tabs of tabss) {
-      const tablist = document.createElement('div')
-      tablist.classList.add('tablist')
-      tablist.setAttribute('role', 'tablist')
-      tablist.setAttribute('aria-label', 'Entertainment')
-      tabs.prepend(tablist)
+      const tabList = document.createElement('div')
+      tabList.classList.add('tab-list')
+      tabList.setAttribute('role', 'tablist')
+      tabList.setAttribute('aria-label', 'Entertainment')
+      tabs.prepend(tabList)
     }
   })()
   const transformationIntoTabs = (() => {
-    const summarys = document.querySelectorAll('.tabs summary')
+    const summarys = document.querySelectorAll('.tabs > details > summary')
     let i = 0
     for (const summary of summarys) {
       i++
       const html = summary.innerHTML,
-            tabsummary = document.createElement('button'),
-            tablist = summary.parentElement.parentElement.firstElementChild // .querySelector('.tablist')
-      tabsummary.id = 'tabsummary-' + i
-      tabsummary.type = 'button'
-      tabsummary.classList.add('tabsummary')
-      tabsummary.setAttribute('role', 'tab')
-      tabsummary.setAttribute('aria-controls', 'tabpanel-' + i)
-      tablist.appendChild(tabsummary) // insertion du bouton
-      tabsummary.insertAdjacentHTML('beforeend', html) // insertion de son contenu
+            tabSummary = document.createElement('button'),
+            tablist = summary.parentElement.parentElement.firstElementChild // .querySelector('.tab-list')
+      tabSummary.id = 'tabsummary-' + i
+      tabSummary.type = 'button'
+      tabSummary.classList.add('tab-summary')
+      tabSummary.setAttribute('role', 'tab')
+      tabSummary.setAttribute('aria-controls', 'tab-panel-' + i)
+      tablist.appendChild(tabSummary) // insertion du bouton
+      tabSummary.insertAdjacentHTML('beforeend', html) // insertion de son contenu
       summary.parentElement.removeChild(summary) // retrait de l'élément d'origine
     }
   })()
-  const currentTab = (() => {
-    const firstSummarys = document.querySelectorAll('.tabs .tabsummary:first-child')
-    for (const firstSummary of firstSummarys) {
-      firstSummary.disabled = true
-      firstSummary.classList.add('current')
-      firstSummary.setAttribute('aria-selected', 'true') // TODO : à vérifier
-    }
-    for (const tabsummary of document.querySelectorAll('.tabs .tabsummary')) {
-      tabsummary.addEventListener('click', () => {
-        const tablist = tabsummary.parentElement.parentElement.firstElementChild // .querySelector('.tablist')
-        //const tabpanels = tabsummary.parentElement.querySelectorAll('.tabpanel')
-        /*
-        for (const tabpanel of tabpanels) {
-          tabpanel.classList.remove('current')
-          tabpanel.setAttribute('aria-hidden', 'true')
-        }
-        */
-        for (const tabsummarySibling of tablist.children) {
-          tabsummarySibling.disabled = false
-          tabsummarySibling.classList.remove('current')
-          tabsummarySibling.setAttribute('aria-selected', 'false')
-        }
-        tabsummary.disabled = true
-        tabsummary.classList.add('current')
-        tabsummary.setAttribute('aria-selected', 'true')
-        /*
-        const tabpanel = document.getElementById(tabsummary.getAttribute('aria-controls'))
-        tabpanel.classList.add('current')
-        tabpanel.setAttribute('aria-hidden', 'false')
-        */
-      })
-    }
-  })()
   const transformationIntoPannels = (() => {
-    const panels = document.querySelectorAll('.tabs details > div')
+    const panels = document.querySelectorAll('.tabs > details > div')
     let i = 0
     for (const panel of panels) {
       i++
-      panel.id = 'tabpanel-' + i
-      panel.classList.add('tabpanel')
+      panel.id = 'tab-panel-' + i
+      panel.classList.add('tab-panel')
       panel.setAttribute('role', 'tabpanel')
       panel.setAttribute('aria-labelledby', 'tabsummary-' + i)
       panel.parentElement.parentElement.appendChild(panel) // déplacement du contenu du panneau
       panel.parentElement.querySelector('details').remove() // retrait de l'élément d'origine
     }
   })()
-  const tabSummarys = document.querySelectorAll('.tabsummary')
-  for (const tabSummary of tabSummarys) tabSummary.addEventListener('click', () => {
-    const tabs = tabSummary.parentElement.parentElement
-    const currentPanel = document.getElementById(tabSummary.getAttribute('aria-controls'))
-    currentPanel.style.display = 'block'
-    for (const panel of tabs.querySelectorAll('.tabpanel')) {
-      if (panel === currentPanel) continue
-      panel.style.display = 'none'
-      if (tabSummary !== tabSummary.classList.contains('current')) {
-      } else {
-        tabSummary.classList.remove('current')
-      }
+  const currentTab = (() => {
+    const firstSummarys = document.querySelectorAll('.tab-list > .tab-summary:first-child')
+    for (const firstSummary of firstSummarys) {
+      firstSummary.disabled = true
+      firstSummary.classList.add('current')
+      firstSummary.setAttribute('aria-selected', 'true') // TODO : à vérifier
     }
-  })
+    for (const tabSummary of document.querySelectorAll('.tab-list > .tab-summary')) {
+      tabSummary.addEventListener('click', () => {
+        const tabList = tabSummary.parentElement.parentElement.firstElementChild // .querySelector('.tab-list')
+        //const tabpanels = tabSummary.parentElement.querySelectorAll('.tab-panel')
+        /*
+        for (const tabpanel of tabpanels) {
+          tabpanel.classList.remove('current')
+          tabpanel.setAttribute('aria-hidden', 'true')
+        }
+        */
+        for (const tabSummarySibling of tabList.children) {
+          tabSummarySibling.disabled = false
+          tabSummarySibling.classList.remove('current')
+          tabSummarySibling.setAttribute('aria-selected', 'false')
+        }
+        tabSummary.disabled = true
+        tabSummary.classList.add('current')
+        tabSummary.setAttribute('aria-selected', 'true')
+        /*
+        const panel = document.getElementById(tabSummary.getAttribute('aria-controls'))
+        panel.classList.add('current')
+        panel.setAttribute('aria-hidden', 'false')
+        */
+      })
+    }
+  })()
+  const currentPanel = (() => {
+    const tabSummarys = document.querySelectorAll('.tab-summary')
+    for (const tabSummary of tabSummarys) tabSummary.addEventListener('click', () => {
+      const panels = tabSummary.parentElement.parentElement.querySelectorAll('.tab-panel') // .firstElementChild.nextSibling.children
+      const currentPanel = document.getElementById(tabSummary.getAttribute('aria-controls'))
+      currentPanel.style.display = 'block'
+      for (const panel of panels) {
+        if (panel === currentPanel) continue
+        panel.style.display = 'none'
+        if (tabSummary !== tabSummary.classList.contains('current')) {
+        } else {
+          tabSummary.classList.remove('current')
+        }
+      }
+    })
+  })()
 })()
