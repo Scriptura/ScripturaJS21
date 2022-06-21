e7eb7d6137452ba61172f2897a8dfe72a669a555
Olivier Chavarin <memoire1@gmail.com>
2022-06-21 22:17:35 +0200

    Liturgical calendar demo updated

--- controllers/calendarDay.js
+++ controllers/calendarDay.js
@@ -10,8 +10,10 @@ router.get('/calendar/:year([0-9]{1,4})/:month(0[1-9]|1[0-2])/:day(0[1-9]|[12][0
     .then(data => {
       if (data === undefined) throw new Error('Error: the query did not return anything because it did not match with data.')
       data._url = req
-      data._previousDay = DateTime.fromFormat(req.params.year + req.params.month + req.params.day, 'yyyyMMdd').minus({days: 1}).toFormat('yyyy/MM/dd')
-      data._nextDay = DateTime.fromFormat(req.params.year + req.params.month + req.params.day, 'yyyyMMdd').plus({days: 1}).toFormat('yyyy/MM/dd')
+      data._previousPage = '/calendar/' + DateTime.fromFormat(req.params.year + req.params.month + req.params.day, 'yyyyMMdd').minus({days: 1}).toFormat('yyyy/MM/dd')
+      data._previousPageInfo = DateTime.fromFormat(req.params.year + req.params.month + req.params.day, 'yyyyMMdd').minus({days: 1}).toFormat('dd/MM/yyyy')
+      data._nextPage = '/calendar/' + DateTime.fromFormat(req.params.year + req.params.month + req.params.day, 'yyyyMMdd').plus({days: 1}).toFormat('yyyy/MM/dd')
+      data._nextPageInfo = DateTime.fromFormat(req.params.year + req.params.month + req.params.day, 'yyyyMMdd').plus({days: 1}).toFormat('dd/MM/yyyy')
       res.render('calendarDay', {data})
     })
     .catch(error => next())
